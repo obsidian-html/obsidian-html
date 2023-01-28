b5dea8eff53ffadb4eb71b2cb38d723a3a79299c
dwrolvink <dwrolvink@gmail.com>
2023-01-28 15:39:38 +0100

    unit tests, add run_all script

---
+++ ci/unit_tests/run_all.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from pathlib import Path
+
+# add /obsidian-html/ci to path
+print(str(Path(os.path.realpath(__file__)).parent.parent))
+sys.path.insert(1, str(Path(os.path.realpath(__file__)).parent.parent))
+
+
+from unit_tests.tests_md_to_html.codeblocks_in_footnote import run_tests as test_codeblocks_in_footnote
+from unit_tests.tests_note_to_md.inline_tags import run_tests as test_inline_tags
+from unit_tests.tests_note_to_md.obs_img_to_md import run_tests as test_obs_img_to_md
+
+os.environ["TESTS_FAILED"] = "0"
+
+test_codeblocks_in_footnote()
+test_inline_tags()
+test_obs_img_to_md()
+
+if (os.environ["TESTS_FAILED"] == '1'):
+    sys.exit(1)
--- ci/unit_tests/tests_note_to_md/inline_tags.py
+++ ci/unit_tests/tests_note_to_md/inline_tags.py
@@ -27,53 +27,60 @@ sys.path.insert(1, str(Path(os.path.realpath(__file__)).parent.parent.parent))
 from unit_tests.unit_test_init import pb, check_test_result, test, \
                                  get_inline_tags
 
-os.environ["TESTS_FAILED"] = "0"
+
+def run_tests():
+    cases = [
+        {   'name'    : 'Test inline tags recognized - "#test"',
+            'function':  get_inline_tags,
+            'set'     : (['#test'], ['test'])
+        },
+        {   'name'    : 'Test inline tags recognized - "#test "',
+            'function':  get_inline_tags,
+            'set'     : (['#test '], ['test'])
+        },
+        {   'name'    : 'Test inline tags recognized - "#test/sub"',
+            'function':  get_inline_tags,
+            'set'     : (['#test/sub'], ['test/sub'])
+        },
+        {   'name'    : 'Test inline tags recognized - "#test.break"',
+            'function':  get_inline_tags,
+            'set'     : (['#test.break'], ['test'])
+        },
+        {   'name'    : 'Test inline tags recognized - "#!invalid"',
+            'function':  get_inline_tags,
+            'set'     : (['#!invalid'], [])
+        },
+        {   'name'    : 'Test inline tags recognized - "#1990"',
+            'function':  get_inline_tags,
+            'set'     : (['#1990'], [])
+        },
+        {   'name'    : 'Test inline tags recognized - "#y1990"',
+            'function':  get_inline_tags,
+            'set'     : (['#!invalid'], [])
+        },
+        {   'name'    : 'Test inline tags recognized - "#1990y"',
+            'function':  get_inline_tags,
+            'set'     : (['#1990y'], ['1990y'])
+        },
+        {   'name'    : 'Test inline tags recognized - "#19_90"',
+            'function':  get_inline_tags,
+            'set'     : (['#19_90'], ['19_90'])
+        },
+    ]
+
+    # for case in cases:
+    #     inp, outp = case['set']
+    #     left = '"' + ', '.join(inp) + '"'
+    #     print(f"{left : <13}", '-->  ', outp)
+
+    for case in cases:
+        test(case)
 
 
-cases = [
-    {   'name'    : 'Test inline tags recognized - "#test"',
-        'function':  get_inline_tags,
-        'set'     : (['#test'], ['test'])
-    },
-    {   'name'    : 'Test inline tags recognized - "#test "',
-        'function':  get_inline_tags,
-        'set'     : (['#test '], ['test'])
-    },
-    {   'name'    : 'Test inline tags recognized - "#test/sub"',
-        'function':  get_inline_tags,
-        'set'     : (['#test/sub'], ['test/sub'])
-    },
-    {   'name'    : 'Test inline tags recognized - "#test.break"',
-        'function':  get_inline_tags,
-        'set'     : (['#test.break'], ['test'])
-    },
-    {   'name'    : 'Test inline tags recognized - "#!invalid"',
-        'function':  get_inline_tags,
-        'set'     : (['#!invalid'], [])
-    },
-    {   'name'    : 'Test inline tags recognized - "#1990"',
-        'function':  get_inline_tags,
-        'set'     : (['#1990'], [])
-    },
-    {   'name'    : 'Test inline tags recognized - "#y1990"',
-        'function':  get_inline_tags,
-        'set'     : (['#!invalid'], [])
-    },
-    {   'name'    : 'Test inline tags recognized - "#1990y"',
-        'function':  get_inline_tags,
-        'set'     : (['#1990y'], ['1990y'])
-    },
-    {   'name'    : 'Test inline tags recognized - "#19_90"',
-        'function':  get_inline_tags,
-        'set'     : (['#19_90'], ['19_90'])
-    },
+if __name__ == "__main__":
+    os.environ["TESTS_FAILED"] = "0"
 
-]
+    run_tests()
 
-for case in cases:
-    inp, outp = case['set']
-    left = '"' + ', '.join(inp) + '"'
-    print(f"{left : <13}", '-->  ', outp)
-
-for case in cases:
-    test(case)
+    if (os.environ["TESTS_FAILED"] == '1'):
+        sys.exit(1)
