4060fbf45a8834be811a02244c559399731e8897
dwrolvink <dwrolvink@gmail.com>
2023-02-03 23:10:00 +0100

    improvements postprocessing code

--- obsidianhtml/post_processing/__init__.py
+++ obsidianhtml/post_processing/__init__.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 
-def convert_markdown_output(pb, convert_function, arg_dict=None):
-    for file in pb.paths['md_folder'].rglob("*.md"):
+def convert_markdown_output(md_folder_path, convert_function, arg_dict=None):
+    for file in Path(md_folder_path).rglob("*.md"):
         with open(file, 'r') as f:
             content = f.read()
         content = convert_function(content, **arg_dict)
