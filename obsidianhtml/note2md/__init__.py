bc56f4214fba05241203e52901479951ff94c3c9
dwrolvink <dwrolvink@gmail.com>
2023-05-17 00:16:51 +0200

    strip hashpart from image link

--- obsidianhtml/note2md/__init__.py
+++ obsidianhtml/note2md/__init__.py
@@ -13,13 +13,13 @@ def obs_img_to_md_img(pb, page):
             parts = matched_link.split("|")
             link = parts.pop(0)
             alias = "|".join(parts)
-            new_link = f"![{alias}](" + urllib.parse.quote(link) + ")"
+            new_link = f"![{alias}](" + urllib.parse.quote(link.split("#")[0]) + ")"
         else:
             new_link = "![](" + urllib.parse.quote(matched_link) + ")"
 
         # Obsidian page inclusions use the same tag...
         # Skip if we don't match image suffixes. Inclusions are handled at the end.
-        link = matched_link.split("|")[0]
+        link = matched_link.split("|")[0].split("#")[0]
         if len(link.split(".")) == 1 or link.split(".")[-1].lower() not in pb.gc("included_file_suffixes", cached=True):
             new_link = f'<inclusion href="{link}" />'
 
